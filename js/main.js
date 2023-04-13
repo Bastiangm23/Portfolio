4890535a02be959c6c442eedee0b670bfba0e1d0
BGM <bastian.garcia.m2@gmail.com>
2023-04-13 13:55:51 -0400

    c

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-navbar = document.querySelector(".navbar");
-
+// Funcion para marcar el border de la barra de navegacion
 window.onscroll = function() {
+    const navbar = document.querySelector(".navbar");
     var y = window.scrollY;
     if (y > 0) {
         navbar.classList.add("sombra-navbar");
     }else{
         navbar.classList.remove ("sombra-navbar");
     }
-
-    console.log(y);
   };
+
+  // Funcion para marcar el porcentaje de la barra de progreso en la seccion de skills
+
+  function porcentajeSkills(porcentaje) {
+    const progressBars = document.querySelectorAll(".progress-bar-inner");
+
+    console.log(progressBars.length);
+    
+  }
